6b32e5240becd175ffacbc79bdb7bf44c3626ec9
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-04-16 14:21:14 +0200

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, FooterComponent, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SettingComponent } from './setting/setting.component';
+import { WebComponent } from './web/web.component';
+import { MobileComponent } from './mobile/mobile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '' , redirectTo:'home' , pathMatch: "full"},
+    
+    //home هياخد full ب pathMatch هنا لو كان ال   => home
+    //home مش هيهمه اي حاجه اهم حاجه تكون الكلمه الاولانيه ب prefix امال لو كان ب => home/setting
+    
+    { path: 'home/setting', component: HomeComponent }, 
+    { path: 'home', component: HomeComponent },
+    
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'setting', component: SettingComponent , children:[
+        { path: '', redirectTo: "web" , pathMatch:"full"},
+        { path: 'web', component: WebComponent },
+        { path: 'mobile', component: MobileComponent },
+    ]
+
+     },
+
+    {path: '**' , component: NotFoundComponent} // لازم تتحط اخر حاجه في الاراي
+
+];
